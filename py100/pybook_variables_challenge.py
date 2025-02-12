7daca89a9dd9aa96bfabb072b84e188c504bce38
Atropos of Nothing <atropos.null@gmail.com>
2025-02-12 12:10:42 +0000

    Saving for the day

---
+++ py100/pybook_variables_challenge.py
@@ -0,0 +1,11 @@
+
+balance = (1000.00 * 1.05 * 1.05 * 1.05 * 1.05 * 1.05) 
+print(balance)
+
+balance_2 = 1000.00
+balance_2 *=1.05
+balance_2 *=1.05
+balance_2 *=1.05
+balance_2 *=1.05
+balance_2 *=1.05
+print(balance_2)
