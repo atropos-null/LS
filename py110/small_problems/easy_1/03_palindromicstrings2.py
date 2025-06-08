cd35a4bd4d9ea3002576c7b298f8f0f036ba80f5
Atropos of Nothing <atropos.null@gmail.com>
2025-06-08 16:59:59 +0000

    2025-06-08

--- py110/small_problems/easy_1/03_palindromicstrings2.py
+++ py110/small_problems/easy_1/03_palindromicstrings2.py
@@ -28,26 +28,22 @@ Algorithm:
 """
 
 def is_real_palindrome(string):
-    comparison_string = ""
     clean_string = ""
    
     for char in string.casefold():
         if char.isalnum():
             clean_string += char
-    
-    for char in clean_string[::-1]:
-        comparison_string += char
 
-    return comparison_string == clean_string
+    return clean_string == clean_string[::-1]
 
-print(is_real_palindrome('madam') == True)          # True
-print(is_real_palindrome('356653') == True)         # True
-print(is_real_palindrome('356635') == False)         # True
-print(is_real_palindrome('356a653') == True)         # True
-print(is_real_palindrome('123ab321') == False)       # True
+print(is_real_palindrome('madam') == True)         # True
+print(is_real_palindrome('356653') == True)        # True
+print(is_real_palindrome('356635') == False)       # True
+print(is_real_palindrome('356a653') == True)       # True
+print(is_real_palindrome('123ab321') == False)      # True
 
 # case doesn't matter
-print(is_real_palindrome('Madam') == True)           # True
+print(is_real_palindrome('Madam') == True)          # True
 
 # only alphanumerics matter
 print(is_real_palindrome("Madam, I'm Adam") == True) # True
