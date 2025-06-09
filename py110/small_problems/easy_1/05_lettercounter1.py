d8ef8c755b3a8207dc729fde8a54d36bdfe7f7dc
Atropos of Nothing <atropos.null@gmail.com>
2025-06-09 07:45:47 +0000

    2025-06-09

---
+++ py110/small_problems/easy_1/05_lettercounter1.py
@@ -0,0 +1,42 @@
+"""
+Problem: Write a function that takes a string consisting of zero or more space-separated words and returns a dictionary 
+that shows the number of words of different sizes.
+    - input: string, sentence structure
+    - output: dictionary, character counts
+    - explicit rules: Words consist of any sequence of non-space characters
+    - implicit rules: punctuation counts towards character count
+
+Example: 'Four score and seven.' == {4: 1, 5: 1, 3: 1, 6: 1}
+
+Data Structure: List to collect and Dictionary for final counts
+
+Algorithm:
+
+- split string at spaces
+- iterate over the list, placing results in the dictionary
+- return the dictionary
+
+"""
+
+def word_sizes(stringie):
+    word_dict = {}
+    split_string = stringie.split()
+    for element in split_string:
+        word_dict[len(element)] = word_dict.get(len(element), 0) + 1
+    return word_dict
+
+
+# All of these examples should print True
+string = 'Four score and seven.'
+print(word_sizes(string) == {4: 1, 5: 1, 3: 1, 6: 1})
+
+string = 'Hey diddle diddle, the cat and the fiddle!'
+print(word_sizes(string) == {3: 5, 6: 1, 7: 2})
+
+string = 'Humpty Dumpty sat on a wall'
+print(word_sizes(string) == {6: 2, 3: 1, 2: 1, 1: 1, 4: 1})
+
+string = "What's up doc?"
+print(word_sizes(string) == {6: 1, 2: 1, 4: 1})
+
+print(word_sizes('') == {})
