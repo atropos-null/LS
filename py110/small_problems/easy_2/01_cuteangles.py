ed82bafb884cd8317ceb82d239091c6283ae76f4
Atropos of Nothing <atropos.null@gmail.com>
2025-06-11 08:56:45 +0000

    2025-6-11

---
+++ py110/small_problems/easy_2/01_cuteangles.py
@@ -0,0 +1,11 @@
+"""
+
+Problem: Write a function that takes a floating point number representing an angle between 0 and 360 degrees and returns 
+a string representing that angle in degrees, minutes, and seconds. 
+    - input: float between 0 and 360
+    - output: angle as a string in degrees, minutes strings.
+    - explicit rules: (°) to represent degrees, a single quote (') to represent minutes, and a double quote (") to represent seconds. 
+    - implicit rules: There are 60 minutes in a degree, and 60 seconds in a minute.
+    - Note: You can use the following constant to represent the degree symbol: DEGREE = "\u00B0"
+
+"""
