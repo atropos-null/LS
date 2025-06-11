983bfcc7ec6c3e964772b73162bcb012a5b5e2fc
Atropos of Nothing <atropos.null@gmail.com>
2025-06-11 09:11:27 +0000

    2025-06-11

---
+++ py110/small_problems/easy_2/02_combininglists.py
@@ -0,0 +1,25 @@
+"""
+Problem: Write a function that takes two lists as arguments and returns a set that contains the union of the values from 
+the two lists. You may assume that both arguments will always be lists.
+    - input: two lists
+    - output: set
+    - explicit rule: lists always assigned
+
+
+Example: ([1, 3, 5], [3, 6, 9]) ==  {1, 3, 5, 6, 9})
+
+Data Structure: None
+
+Algorithm
+    - convert lists to sets
+    - use | to join the set
+    - return new set
+
+"""
+
+def union(lst1, lst2):
+    set1 = set(lst1)
+    set2 = set(lst2)
+    return set1 | set2
+
+print(union([1, 3, 5], [3, 6, 9]) == {1, 3, 5, 6, 9}) # True
