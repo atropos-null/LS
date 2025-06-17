7238a7cd39dcd448d6740026501b1839697788b0
Atropos of Nothing <atropos.null@gmail.com>
2025-06-17 11:57:20 +0000

    2025-06-17

---
+++ py110/small_problems/easy_2/07_multiplylists.py
@@ -0,0 +1,34 @@
+"""
+
+Problem: Write a function that takes two list arguments, each containing a list of numbers, and returns a new list that 
+contains the product of each pair of numbers from the arguments that have the same index. You may assume that the arguments 
+contain the same number of elements.
+
+    - input: 2 lists
+    - output: 1 list with numbers of each list multiplied to each element position
+    - rules: arguments have the same number of elements
+
+Data Structure: List
+
+Algorithm:  
+    - initialize empty list
+    - zip up two lists
+    - convert zip to list
+    - multiply the elements of the tuple
+    - add product to empty list
+    - return list
+
+
+"""
+
+def multiply_list(array1, array2):
+    prod_list = []
+    temp_list = list(zip(array1, array2))
+    for element in temp_list:
+        product = element[0] * element[1]
+        prod_list.append(product)
+    return prod_list
+
+list1 = [3, 5, 7]
+list2 = [9, 10, 11]
+print(multiply_list(list1, list2) == [27, 50, 77])  # True
