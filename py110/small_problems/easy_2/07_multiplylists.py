8dcd7c2f2e3cbbc60d8729cf31436ad993f45fff
Atropos of Nothing <atropos.null@gmail.com>
2025-06-17 12:07:57 +0000

    2025-06-17

--- py110/small_problems/easy_2/07_multiplylists.py
+++ py110/small_problems/easy_2/07_multiplylists.py
@@ -32,3 +32,18 @@ def multiply_list(array1, array2):
 list1 = [3, 5, 7]
 list2 = [9, 10, 11]
 print(multiply_list(list1, list2) == [27, 50, 77])  # True
+
+
+"""
+Further Optimization:
+
+def multiply_list(array1, array2):
+    prod_list = []
+    for a, b in zip(array1, array2):
+        prod_list.append(a * b)
+    return prod_list
+
+def multiply_list(array1, array2):
+    return [a * b for a, b in zip(array1, array2)]
+
+"""
---
+++ py110/small_problems/easy_2/08_listofdigits.py
@@ -0,0 +1,33 @@
+"""
+
+Problem: Write a function that takes one argument, a positive integer, and returns a list of the digits in the number.
+    - input: a positive integer
+    - output: a list of digits in the number.
+
+Example:  12345 = [1,2,3,4,5]
+
+Data Structure: List
+
+Algorithm:
+    - initialize empty list
+    - coerce integer to a string
+    - for loop:
+        - split string
+        - recoerce back to an integer
+        - append list
+    
+
+"""
+
+def digit_list(number):
+    result = []
+    string = str(number)
+    for char in string:
+        result.append(int(char))
+    return result
+
+
+print(digit_list(12345) == [1, 2, 3, 4, 5])       # True
+print(digit_list(7) == [7])                       # True
+print(digit_list(375290) == [3, 7, 5, 2, 9, 0])   # True
+print(digit_list(444) == [4, 4, 4])               # True
