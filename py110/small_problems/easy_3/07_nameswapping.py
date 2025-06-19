34d6cb444fa4dd9cd5d8eae6181c51ed62199ace
Atropos of Nothing <atropos.null@gmail.com>
2025-06-19 09:50:55 +0000

    2025-06-19

--- py110/small_problems/easy_3/07_nameswapping.py
+++ py110/small_problems/easy_3/07_nameswapping.py
@@ -34,3 +34,18 @@ def swap_name(name):
 
 print(swap_name('Joe Roberts') == "Roberts, Joe")   # True
 print(swap_name('Karl Oskar Henriksson Ragvals') == "Ragvals, Karl Oskar Henriksson")  # True
+
+"""
+
+Further Optimization:
+def swap_name(name):
+    working = name.split()
+    last_name = working[-1]
+    first_name = working[0]
+    middle_name = " ".join(working[1:-1])
+
+    if middle_name:
+        return f"{last_name}, {first_name} {middle_name}"
+    return f"{last_name}, {first_name}"
+
+"""
---
+++ py110/small_problems/easy_3/08_sequencecount.py
@@ -0,0 +1,56 @@
+"""
+
+Problem: Create a function that takes two integers as arguments. The first argument is a count, and 
+the second is the starting number of a sequence that your function will create. The function should 
+return a list containing the same number of elements as the count argument. The value of each element 
+should be a multiple of the starting number.
+
+You may assume that count will always be an integer greater than or equal to 0. The starting number can 
+be any integer. If the count is 0, the function should return an empty list.
+
+    - input: 2 integers
+    - output: a list
+
+Example: 5, 1 ==> [1, 2, 3, 4, 5]
+
+Data Structure: List
+
+Algo:
+    - def count, start
+    - initialize empty list
+    - for i in range (start, count+1)
+        empty_list.append(i)
+    - return list
+
+"""
+
+def sequence(count, start):
+    result = []
+    stop = (start * count)
+    if start < 0:
+        for i in range(start, stop - 1, start):
+            result.append(i)
+    elif start == 0:
+        for i in range(count):
+            result.append(start)
+    else:
+        for i in range(start, stop + 1, start):
+            result.append(i)
+
+    print(result)
+    return result
+    
+print(sequence(5, 1) == [1, 2, 3, 4, 5])          # True
+print(sequence(4, -7) == [-7, -14, -21, -28])     # True
+print(sequence(3, 0) == [0, 0, 0])                # True
+print(sequence(0, 1000000) == [])                 # True
+
+
+""" 
+
+Further optimization:
+
+def sequence(count, start_num):
+    return [start_num * num for num in range(1, count + 1)]
+    
+"""
