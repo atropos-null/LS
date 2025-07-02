cdbeebed0a510068245de081b45c73650d56dbd2
Atropos of Nothing <atropos.null@gmail.com>
2025-07-02 08:49:37 +0000

    2025-07-02

--- py110/small_problems/easy_4/02_mergesets.py
+++ py110/small_problems/easy_4/02_mergesets.py
@@ -18,9 +18,7 @@ Algorithm:
 """
 
 def merge_sets(lst1, lst2):
-    set1 = set(lst1)
-    set2 = set(lst2)
-    return set1 | set2
+    return set(lst1) | set(lst2)
 
 list1 = [3, 5, 7, 9]
 list2 = [5, 7, 11, 13]
