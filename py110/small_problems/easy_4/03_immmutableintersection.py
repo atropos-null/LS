fc3f7783e1d1029b1bd54748240d7adbfaf92bdf
Atropos of Nothing <atropos.null@gmail.com>
2025-07-02 08:58:41 +0000

    2025-07-02

---
+++ py110/small_problems/easy_4/03_immmutableintersection.py
@@ -0,0 +1,24 @@
+"""
+Problem: Transform two lists into frozen sets and find their common elements.
+    - input: 2 lists
+    - output: 1 frozen set
+
+Example: [2, 4, 6, 8] and [1, 3, 5, 7, 8] become frozenset({8})
+
+Data Structure: None
+
+Algorithm:
+    - convert list to set
+    - union the set: & symbol
+    - convert to frozen set
+
+"""
+
+
+def intersection(list1, list2):
+    return frozenset(set(list1) & set(list2))
+
+list1 = [2, 4, 6, 8]
+list2 = [1, 3, 5, 7, 8]
+expected_result = frozenset({8})
+print(intersection(list1, list2) == expected_result) # True
