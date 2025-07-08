0fa16f135dc135cf9c0603651e315376723cd2fb
Atropos of Nothing <atropos.null@gmail.com>
2025-07-08 17:21:55 +0000

    2025-07-08

---
+++ py110/small_problems/debugging_01/02_reverseastring.py
@@ -0,0 +1,8 @@
+def reverse_string(string):
+    new_string = "" #there was no string to capture the new order
+    for char in string:
+        new_string = char + new_string 
+        
+    return new_string #or, new_string[::-1]
+
+print(reverse_string("hello") == "olleh")
