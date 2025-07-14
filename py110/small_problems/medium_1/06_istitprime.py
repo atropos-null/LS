0aec763fa33e21d483a4f09397e19e045646ce48
Atropos of Nothing <atropos.null@gmail.com>
2025-07-14 09:03:19 +0000

    2025-07-14

---
+++ py110/small_problems/medium_1/06_istitprime.py
@@ -0,0 +1,43 @@
+"""
+A prime number is a positive number that is evenly divisible only by itself and 1. 
+Thus, 23 is prime since its only divisors are 1 and 23. However, 24 is not prime since it has divisors of 
+1, 2, 3, 4, 6, 8, 12, and 24. Note that the number 1 is not prime.
+
+Problem: Write a function that...
+    - input:  takes a positive integer as an argument
+    - output: returns True if the number is prime, False if it is not prime.
+    - rules: You may not use any of Python's add-on packages to solve this problem. 
+    
+Example 1 == False
+
+Data Structure: None
+
+Algorithm:
+
+"""
+
+def is_prime(integer):
+    if integer == 1:
+        return False
+    for i in range(2, integer):
+        if integer % i == 0 and i != integer:
+            return False
+    return True
+
+
+print(is_prime(1) == False)              # True
+print(is_prime(2) == True)               # True
+print(is_prime(3) == True)               # True
+print(is_prime(4) == False)              # True
+print(is_prime(5) == True)               # True
+print(is_prime(6) == False)              # True
+print(is_prime(7) == True)               # True
+print(is_prime(8) == False)              # True
+print(is_prime(9) == False)              # True
+print(is_prime(10) == False)             # True
+print(is_prime(23) == True)              # True
+print(is_prime(24) == False)             # True
+print(is_prime(997) == True)             # True
+print(is_prime(998) == False)            # True
+print(is_prime(3_297_061) == True)       # True
+print(is_prime(23_297_061) == False)     # True
