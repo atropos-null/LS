312ea09529c14a68940a3274fa44264903224257
Atropos of Nothing <atropos.null@gmail.com>
2025-07-15 09:52:38 +0000

    2025-07-15

---
+++ py110/small_problems/medium_2/02_trianglesides.py
@@ -0,0 +1,53 @@
+"""
+Problem: Write a function that takes the lengths of the three sides of a triangle as arguments and returns one of the 
+following four strings representing the triangle's classification: 'equilateral', 'isosceles', 'scalene', or 'invalid'.
+
+Equilateral: All three sides have the same length.
+Isosceles: Two sides have the same length, while the third is different.
+Scalene: All three sides have different lengths.
+
+Example: 3, 3, 3 = equilateral
+
+Data Structure: None
+
+Algorithm: 
+
+if side1 = side2 = side 3:
+    return "equilateral
+
+elif side1 = side2 or side1 = side3 or side2 = side3:
+    return "isoceles"
+
+elif side1 != side2 != side3:
+    return "scalene"
+
+else:
+    return "invalid"
+"""
+
+def triangle(side1, side2, side3):
+
+    if side1 <= 0 or side2 <= 0 or side3 <= 0:
+        return "invalid"
+    
+    elif side1 + side2 <= side3 or side2 + side3 <= side1 or side1 + side3 <= side2:
+        return "invalid"
+
+    elif side1 == side2 == side3:
+        return "equilateral"
+
+    elif side1 == side2 or side1 == side3 or side2 == side3:
+        return "isosceles"
+
+    elif side1 != side2 != side3:
+        return "scalene"
+
+    else:
+        return "invalid"
+
+
+print(triangle(3, 3, 3) == "equilateral")  # True
+print(triangle(3, 3, 1.5) == "isosceles")  # True
+print(triangle(3, 4, 5) == "scalene")      # True
+print(triangle(0, 3, 3) == "invalid")      # True
+print(triangle(3, 1, 1) == "invalid")      # True
